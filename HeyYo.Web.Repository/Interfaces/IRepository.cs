34257f70b5783fee6cbf9a5b2557fbf174956806
Gabi Lapadat <lapadatrobert123@gmail.com>
2020-07-29 01:00:38 +0300

    Added base generic repository that all other repositories can inherit from.

---
+++ HeyYo.Web.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HeyYo.Web.Repository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task Add(T model);
+
+        Task AddRange(IEnumerable<T> models);
+
+        Task<T> GetById<Id>(Id id);
+
+        Task Remove(T model);
+
+        Task RemoveRange(IEnumerable<T> models);
+
+        Task Update(T models);
+
+        Task UpdateRange(IEnumerable<T> models);
+    }
+}
---
+++ HeyYo.Web.Repository/Repositories/Repository.cs
@@ -0,0 +1,64 @@
+﻿using HeyYo.Web.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HeyYo.Web.Repository.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected DbContext _context;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(T model)
+        {
+            await _context.AddAsync(model);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddRange(IEnumerable<T> models)
+        {
+            await _context.AddRangeAsync(models);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> GetById<Id>(Id id)
+        {
+            return await _context.FindAsync<T>(id);
+        }
+
+        public async Task Remove(T model)
+        {
+            _context.Remove(model);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveRange(IEnumerable<T> models)
+        {
+            _context.RemoveRange(models);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(T model)
+        {
+            _context.Update(model);
+
+            await _context.SaveChangesAsync();
+        }       
+
+        public async Task UpdateRange(IEnumerable<T> models)
+        {
+            _context.UpdateRange(models);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
